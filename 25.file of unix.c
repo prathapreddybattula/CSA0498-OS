0f9eafd812db0745c49685808e0b524b25554edc
prathapreddybattula <112234878+prathapreddybattula@users.noreply.github.com>
2023-02-06 09:02:34 +0530

    Create 25.file of unix.c

---
+++ 25.file of unix.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+FILE *fp;
+char ch;
+fp = fopen("hello.txt", "w");
+printf("Enter data");
+while( (ch = getchar()) != EOF) {
+  putc(ch,fp);
+}
+fclose(fp);
+fp = fopen("hello.txt", "r");
+
+while( (ch = getc(fp)! = EOF)
+  printf("%c",ch);
+  
+fclose(fp);
+}
